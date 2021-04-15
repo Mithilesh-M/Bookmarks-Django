8cd94089cc35f6c7248a4885232e8bac14122068
Mithilesh-M <msmithilesh@yahoo.in>
2021-04-15 15:16:58 +0530

    Add models for bookmark application
    Add models and model fields for bookmark app

--- bookmarkproject/settings.py
+++ bookmarkproject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bookmarksapp.apps.BookmarksappConfig',
+    'taggit',
 ]
 
 MIDDLEWARE = [
--- bookmarksapp/models.py
+++ bookmarksapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
-# Create your models here.
+
+class Event(models.Model):
+    """Model representing a Event."""
+    name = models.CharField(max_length=200, help_text='Enter a webpage name')
+    url = models.CharField(max_length=200, help_text='Enter a webpage url')
+    description = models.CharField(max_length=200, help_text='Enter a webpage description')
+    tags = TaggableManager(help_text='Enter webpage tags')
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
