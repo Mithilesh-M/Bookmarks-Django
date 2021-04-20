562c1d15e0d11ab6dc745e45956f5a6f73db391b
Mithilesh-M <msmithilesh@yahoo.in>
2021-04-20 16:32:54 +0530

    Update search bar filter function to bookmarks app
    Updated search filter

--- bookmarksapp/filters.py
+++ bookmarksapp/filters.py
@@ -3,6 +3,8 @@ from .models import Bookmarks
 
 
 class BookmarksFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(lookup_expr='icontains')
+
     class Meta:
         model = Bookmarks
         fields = ["name"]
